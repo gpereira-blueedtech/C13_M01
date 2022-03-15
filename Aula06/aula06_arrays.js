056bdb71681993d21972de38338e40bc3818fe8f
Gabriel Pereira <gpereira@blueedtech.com.br>
2022-03-14 21:26:52 -0300

    Aula 06

---
+++ Aula06/aula06_arrays.js
@@ -0,0 +1,16 @@
+console.clear();
+const prompt = require('prompt-sync')();
+
+let aluno = 'Gabriel';
+let aluno01 = 'Fernando';
+let aluno02 = 'Tiago';
+let professor = 'Gabriel Pereira'
+let professora = ['Duda', 'Londrina']
+
+// Uma lista é capaz de armazenar vários elementos, cada um separado por vírgula.
+// Eu posso armazenar qualquer elemento, uma string, número, outra variável e até outra array
+let alunos = ['Gabriel' , 'Fernando' , 'Tiago' , 20 , 13 , 30 , professor, ['C13','Blue'], professora];
+
+console.log(alunos);
+
+console.log();

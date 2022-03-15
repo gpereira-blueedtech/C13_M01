a43813d97d61087dcc1f650166be866ee647e907
Gabriel Pereira <gpereira@blueedtech.com.br>
2022-03-14 22:29:32 -0300

    Aula 06

--- Aula06/aula06_arrays.js
+++ Aula06/aula06_arrays.js
@@ -1,9 +1,8 @@
 console.clear();
 const prompt = require('prompt-sync')();
 
-let aluno = 'Gabriel';
-let aluno01 = 'Fernando';
-let aluno02 = 'Tiago';
+/*
+
 let professor = 'Gabriel Pereira'
 let professora = ['Duda', 'Londrina']
 
@@ -12,5 +11,54 @@ let professora = ['Duda', 'Londrina']
 let alunos = ['Gabriel' , 'Fernando' , 'Tiago' , 20 , 13 , 30 , professor, ['C13','Blue'], professora];
 
 console.log(alunos);
+
+*/
+
+
+// let filmes = ["Jurassic Park","O Senhor dos Aneis","Harry Potter"];
+
+// Mesma coisa que a de cima, mas com uma visualização melhor
+let filmes = [
+    "Jurassic Park", 
+    "O Senhor dos Aneis", 
+    "Harry Potter",
+    [1993, 2002, 2001] // Uma array conta como apenas um elemento.
+];
+
+// // Verificando o número de elementos de uma array
+// console.log(filmes.length);
+// console.log();
+
+// // Verificando o número de elementos de um elemento dentro da array
+// console.log(filmes[3].length);
+// console.log();
+
+// // Retornando um elemento específico
+// // Lembrando que o primeiro indíce é sempre o 0!!
+// console.log(filmes[3]);
+
+// // Retornando o indíce 0 da array no índice 3 da principal.
+// console.log(filmes[3][0]);
+
+// console.log(filmes);
+
+let novofilme = prompt('Digite o nome do filme: ')
+filmes.push(novofilme);
+console.log();
+
+let novoano = +prompt('Digite o ano do filme: ');
+filmes[3].push(novoano) // Adicionando uma array à array de dentro (elemento 3)
+
+console.log(filmes);
+
+
+// let filmes2 = [];
+
+// while (filmes2.length < 5){
+//     let filme2novo = prompt('Digite o filme: ');
+//     filmes2.push(filme2novo)
+// }
+
+// console.log(filmes2);
 
 console.log();
